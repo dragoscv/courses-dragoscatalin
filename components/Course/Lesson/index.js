e4e102cd9dad80f49aebdacc3557bac6cdd4cc11
Dragos Catalin <dragoscv12@gmail.com>
2022-12-27 01:40:40 +0200

    player with controls

--- components/Course/Lesson/index.js
+++ components/Course/Lesson/index.js
@@ -11,6 +11,115 @@ const Lesson = () => {
     const router = useRouter()
     const { courseId, lessonId } = router.query
     const [lesson, setLesson] = React.useState(null)
+    //set state for lesson's video controls
+    const [controls, setControls] = React.useState(false)
+    const [playing, setPlaying] = React.useState(true)
+    const [muted, setMuted] = React.useState(false)
+    const [volume, setVolume] = React.useState(0.8)
+    const [played, setPlayed] = React.useState(0)
+    const [loaded, setLoaded] = React.useState(0)
+    const [duration, setDuration] = React.useState(0)
+    const [loop, setLoop] = React.useState(false)
+    const [playbackRate, setPlaybackRate] = React.useState(1.0)
+    const [pip, setPip] = React.useState(false)
+    const [light, setLight] = React.useState(false)
+    const player = React.useRef(null)
+
+    const handlePlayPause = () => {
+        setPlaying(!playing)
+    }
+
+    const handleStop = () => {
+        setUrl(null)
+        setPlayed(0)
+        setLoaded(0)
+        setPlaying(false)
+    }
+
+    const handleToggleControls = () => {
+        setUrl(null)
+        setControls(!controls)
+    }
+
+    const handleToggleLight = () => {
+        setLight(!light)
+    }
+
+    const handleToggleLoop = () => {
+        setLoop(!loop)
+    }
+
+    const handleVolumeChange = (e) => {
+        setVolume(parseFloat(e.target.value))
+    }
+
+    const handleToggleMuted = () => {
+        setMuted(!muted)
+    }
+
+    const handleSetPlaybackRate = (e) => {
+        setPlaybackRate(parseFloat(e.target.value))
+    }
+
+    const handleTogglePIP = () => {
+        setPip(!pip)
+    }
+
+    const handlePlay = () => {
+        console.log('onPlay')
+        setPlaying(true)
+    }
+
+    const handleEnablePIP = () => {
+        console.log('onEnablePIP')
+        setPip(true)
+    }
+
+    const handleDisablePIP = () => {
+        console.log('onDisablePIP')
+        setPip(false)
+    }
+
+    const handlePause = () => {
+        console.log('onPause')
+        setPlaying(false)
+    }
+
+    const handleSeekMouseDown = (e) => {
+        setSeeking(true)
+    }
+
+    const handleSeekChange = (e) => {
+        setPlayed(parseFloat(e.target.value))
+    }
+
+    const handleSeekMouseUp = (e) => {
+        setSeeking(false)
+        player.current.seekTo(parseFloat(e.target.value))
+    }
+
+    const handleProgress = (state) => {
+        console.log('onProgress', state)
+        // We only want to update time slider if we are not currently seeking
+        if (!seeking) {
+            setPlayed(state.played)
+        }
+        setLoaded(state.loaded)
+    }
+
+    const handleEnded = () => {
+        console.log('onEnded')
+        setPlaying(loop)
+    }
+
+    const handleDuration = (duration) => {
+        console.log('onDuration', duration)
+        setDuration(duration)
+    }
+
+    const handleToggleFullscreen = () => {
+        screenfull.toggle(player)
+    }
 
     React.useEffect(() => {
         if (!courseId || !lessonId) return
@@ -39,12 +148,61 @@ const Lesson = () => {
                         <h2>{lesson.title}</h2>
                         <p>{lesson.description}</p>
                         <p>{moment(lesson.createdAt.toDate()).format('DD/MM/YYYY')}</p>
-                        <ReactPlayer
-                            width="400px"
-                            playing
-                            onReady={() => console.log('onReady')}
-                            url={[{ src: lesson.video, type: 'video/webm' }]}
-                        />
+                        <div className='player-wrapper'>
+                            <ReactPlayer
+                                ref={player}
+                                width="100%"
+                                height="100%"
+                                playing={playing}
+                                controls={controls}
+                                light={light}
+                                loop={loop}
+                                playbackRate={playbackRate}
+                                volume={volume}
+                                muted={muted}
+                                pip={pip}
+                                onPlay={handlePlay}
+                                onEnablePIP={handleEnablePIP}
+                                onDisablePIP={handleDisablePIP}
+                                onPause={handlePause}
+                                onBuffer={() => console.log('onBuffer')}
+                                onSeek={e => console.log('onSeek', e)}
+                                onEnded={handleEnded}
+                                onError={e => console.log('onError', e)}
+                                onProgress={handleProgress}
+                                onDuration={handleDuration}
+                                onReady={() => console.log('onReady')}
+                                url={[{ src: lesson.video, type: 'video/webm' }]}
+                            />
+                        </div>
+                        <div className='player-controls'>
+                            <button onClick={handlePlayPause}>{playing ? 'Pause' : 'Play'}</button>
+                            <button onClick={handleStop}>Stop</button>
+                            <button onClick={handleToggleControls}>Toggle Controls</button>
+                            <button onClick={handleToggleLight}>Toggle Light</button>
+                            <button onClick={handleToggleLoop}>Toggle Loop</button>
+                            <button onClick={handleToggleMuted}>{muted ? 'Unmute' : 'Mute'}</button>
+                            <button onClick={handleTogglePIP}>{pip ? 'Disable PiP' : 'Enable PiP'}</button>
+                            <button onClick={handleToggleFullscreen}>Toggle Fullscreen</button>
+                            <input
+                                type='range' min={0} max={0.999999} step='any'
+                                value={played}
+                                onMouseDown={handleSeekMouseDown}
+                                onChange={handleSeekChange}
+                                onMouseUp={handleSeekMouseUp}
+                            />
+                            <input
+                                type='range' min={0} max={1} step='any'
+                                value={volume}
+                                onChange={handleVolumeChange}
+                            />
+                            <input
+                                type='range' min={0.5} max={2} step='any'
+                                value={playbackRate}
+                                onChange={handleSetPlaybackRate}
+                            />
+                        </div>
+                        
                     </>
                 )}
             </div>
